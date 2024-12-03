bdb87e904f7e36dc04e99c7f958b7b94896fc93d
LinaMVG <limarcegarzon@gmail.com>
2024-12-03 07:08:34 -0500

    add footer and styles - theme

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import { useContextGlobal } from './utils/global.context';
+import "../Styles/footer.css"
 
 const Footer = () => {
+  const { state } = useContextGlobal();
+
   return (
-    <footer>
-        <p>Powered by</p>
-        <img src="./img/DH.png" alt='DH-logo' />
+    <footer className={`footer footer-${state.theme}`}>
+
+      <div className="footer-top">
+        <p>FrontEnd III DH 2024 - Elizabeth Peña & Lina M Velásquez - Camada 4</p>
+      </div>
+
+      <div className="footer-content">
+        <div className="footer-powered">
+          <p>Powered by</p>
+          <img src="/images/DH.png" alt="DH-logo" className="footer-logo-img" />
+          </div>
+        
+        <div className="footer-icons">
+            <a href="#" target="_blank" rel="noopener noreferrer">
+            <img src="public\images\ico-facebook.png" alt='Facebook' className="icon"/>
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+            <img src="public\images\ico-instagram.png" alt='Instagram' className="icon"/>
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+            <img src="public\images\ico-tiktok.png" alt='TikTok' className="icon"/>
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+            <img src="public\images\ico-whatsapp.png" alt='WhatsApp' className="icon"/>
+            </a>
+        </div>
+      </div>
+
     </footer>
   )
 }
